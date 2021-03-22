e860a67d302d6f78eb0d00946b5241216bd53f1e
KamranHussain05 <38573620+KamranHussain05@users.noreply.github.com>
2021-03-22 11:05:55 -0700

    Added New Project: CarController lesson

---
+++ lsn_CarController/src/BasicCarModel.java
@@ -0,0 +1,44 @@
+
+public class BasicCarModel
+{
+//   public String myMoveState;
+//   public String myCarName;
+   
+   private String myMoveState;
+   private String myCarName;
+   
+   
+   public BasicCarModel(String name)
+   {
+      myCarName = name; 
+      this.stop();
+   }
+   
+   public void move()
+   {
+      myMoveState = "FORWARD";
+      // moving code goes here
+      System.out.println(myCarName + " car class code is making wheels move...");
+   }
+   
+   public void stop()
+   {
+      myMoveState = "STOP";
+      // stop code goes here
+      System.out.println(myCarName + " car class code is making wheels stop...");
+   }
+   
+   public void setCarName(String name)
+   {
+      myCarName = name;
+   }
+   
+   public String getCarName()
+   {
+      return myCarName;
+   }
+   public String getMoveState()
+   {
+      return myMoveState;
+   }
+}
---
+++ lsn_CarController/src/CarController.java
@@ -0,0 +1,31 @@
+
+public class CarController
+{
+
+   public static void main(String[] args)
+   {
+      // working with hte basic model
+      
+      System.out.println("Instantiating a new car");
+      BasicCarModel car1 = new BasicCarModel("Mr. Taylor's Kia");
+      System.out.println("the cars name is : " + car1.getCarName());
+      System.out.println(car1.getCarName() + " move state is " + car1.getMoveState());
+      
+      System.out.println("Controller making car move");
+      car1.move(); // move the car
+      System.out.println(car1.getCarName() + " move state is " + car1.getMoveState());
+      
+      System.out.println("Controller changing car name");
+      car1.setCarName("Angel's Kia"); // change the car's name
+      System.out.println(car1.getCarName() + " move state is " + car1.getMoveState());
+      
+      System.out.println("Controller stopping car");
+      car1.stop(); // stopping the car
+      
+      String name = car1.myMoveState;
+      car1.myMoveState = "whatever...";
+      System.out.println(car1.getCarName() + " move state is " + car1.getMoveState());
+
+   }
+
+}
